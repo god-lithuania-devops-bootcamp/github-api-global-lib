cefd400c418ad453d32f35ade0032a2165a39e1e
Ramunas <123743206+ramunvaitkev@users.noreply.github.com>
2023-04-26 18:00:00 +0300

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
     loadLinuxScript(name: 'hello-world.sh')
-    sh "./hello-world.sh ${config.name} ${config.dayOfWeek} ${config.time}"
+    sh "./hello-world.sh ${config.name} ${config.dayOfWeek} ${config.time} ${config.shared_lib}"
 }
